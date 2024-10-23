6d687bd38b0c8df839c3ff8133e2ef088fa5458c
Angi-Adema <dr.adema@gmail.com>
2024-10-23 12:42:28 -0600

    saved new file and logic for UserDetailsRepository.java

--- src/main/java/com/in28minutes/springboot/first_rest_api/user/UserDetailsRepository.java
+++ src/main/java/com/in28minutes/springboot/first_rest_api/user/UserDetailsRepository.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.first_rest_api.user;
 
-public interface UserDetailsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDetailsRepository extends JpaRepository<UserDetails, Long>{  // Pass in UserDetails and the type of the id field.
 
 }
