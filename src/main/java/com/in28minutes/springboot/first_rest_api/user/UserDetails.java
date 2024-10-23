2cf965be675f6a78ddf6a5142f097c492a9e6901
Angi-Adema <dr.adema@gmail.com>
2024-10-23 12:41:33 -0600

    updated pom.xml to include h2database, spring-boot-starter-data-jpa and spring-boot-starter-data-rest, configured the database URL in application.properties and updated the logging to info, created the user packaage with the UserDetails.java and UserDetailsRepository.java classes

---
+++ src/main/java/com/in28minutes/springboot/first_rest_api/user/UserDetails.java
@@ -0,0 +1,46 @@
+package com.in28minutes.springboot.first_rest_api.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+// We want this to be an entity that is managed by JPA so we use @Entity
+@Entity
+public class UserDetails {  // We want to add users, delete users, etc.
+
+	// Store a few user details.
+	// To have JPA generate the id for us we add @Id and @Generated. Spring uses this to generate the USER_DETAILS table.
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private String role;
+	
+	// Generate a constructor to initialize the fields.
+	public UserDetails(String name, String role) {
+		super();
+		this.name = name;
+		this.role = role;
+	}
+
+	// Generate getter methods.
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	// Generate the toString() method.
+	@Override
+	public String toString() {
+		return "UserDetails [id=" + id + ", name=" + name + ", role=" + role + "]";
+	}
+	
+
+}
---
+++ src/main/java/com/in28minutes/springboot/first_rest_api/user/UserDetailsRepository.java
@@ -0,0 +1,5 @@
+package com.in28minutes.springboot.first_rest_api.user;
+
+public interface UserDetailsRepository {
+
+}
