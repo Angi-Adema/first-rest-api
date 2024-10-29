b085cb995cc321b4561f3a7b7b1ae285a430caf4
Angi-Adema <dr.adema@gmail.com>
2024-10-29 16:08:40 -0600

    added SurveyResourceTest.java to add unit test. Added logic for the test to retrieve a specific survey question both in a 404 and 200 scenario and confirmed a returned JSON response.

--- src/test/java/com/in28minutes/springboot/first_rest_api/survey/SurveyResourceTest.java
+++ src/test/java/com/in28minutes/springboot/first_rest_api/survey/SurveyResourceTest.java
@@ -1,9 +1,13 @@
 // First unit test. Unit test files end in 'Test' and integration test files end in 'IT'.
 package com.in28minutes.springboot.first_rest_api.survey;
 
-import java.awt.PageAttributes.MediaType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -40,16 +44,63 @@ class SurveyResourceTest {
 		// We want a JSON response back so we add in .accept().
 		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(SPECIFIC_QUESTION_URL).accept(org.springframework.http.MediaType.APPLICATION_JSON);
 		
-		// To do a .perform, we must have a RequestBuilder. CTRL + 1 to add to local variable.
+		// To do a .perform, we must have a RequestBuilder. CTRL + 1 to add to local variable (see it above).
 		MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
 		
 		// Confirm result being returned.
-		System.out.println(mvcResult.getResponse().getContentAsString());
+		//System.out.println(mvcResult.getResponse().getContentAsString());
+		
+		// Use the assert to confirm results.
+		assertEquals(404, mvcResult.getResponse().getStatus());
 		
 		// Status comes back as 404 because in SurveyResource.java we are using a mock of surveyService but we did not set a value for specific method call. (Question question) Error handling says to respond 404.
-		System.out.println(mvcResult.getResponse().getStatus());  
+		//System.out.println(mvcResult.getResponse().getStatus());  // Coming in as 404
 		
 		// We do not want to test for a 404 scenario though. Need to make the method Mock -> surveyService.retrieveSpecificSurveyQuestion(surveyId, questionId) become a specific bean.
+		
+	}
+	
+	@Test
+	// Grab name of method from SurveyResource.java
+	void retrieveSpecificSurveyQuestion_basicScenario() throws Exception {
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(SPECIFIC_QUESTION_URL).accept(org.springframework.http.MediaType.APPLICATION_JSON);
+		
+		// This is the question we want to see returned in JSON format.
+		// We want to return a specific bean back. In this example, Question1. Because this is a unit test, ServeyService.java will not be launched up at all so we copy and paste it here.
+		Question question = new Question("Question1", "Most Popular Cloud Platform Today",
+				Arrays.asList("AWS", "Azure", "Google Cloud", "Oracle Cloud"), "AWS");
+		
+		// We want to mock this method: Mock -> surveyService.retrieveSpecificSurveyQuestion(surveyId, questionId)
+		// When the method inside of when below is called, we want to return this question back. Then we can check the response to be sure Question1 is the one coming back using the when method above.
+		when(surveyService.retrieveSpecificSurveyQuestion("Survey1", "Question1")).thenReturn(question);
+		
+		// To do a .perform, we must have a RequestBuilder. CTRL + 1 to add to local variable (see it above).
+		MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+		
+		String expectedResponse = """
+				{
+					"id":"Question1",
+					"description":"Most Popular Cloud Platform Today",
+					"options":["AWS","Azure","Google Cloud","Oracle Cloud"],
+					"correctAnswer":"AWS"
+				}
+				
+				""";
+		
+		// Confirm result being returned in the JSON format.
+		//{"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"}
+		//System.out.println(mvcResult.getResponse().getContentAsString());
+		
+		// Status comes back as 404 because in SurveyResource.java we are using a mock of surveyService but we did not set a value for specific method call. (Question question) Error handling says to respond 404.
+		//System.out.println(mvcResult.getResponse().getStatus()); // Coming in as 200 
+		
+		// Use the assert to confirm results.
+		assertEquals(200, mvcResult.getResponse().getStatus());
+		
+		// To also check the content of the response, we use JsonAssert.
+		JSONAssert.assertEquals(expectedResponse, mvcResult.getResponse().getContentAsString(), false);
+		
 		
 	}
 
